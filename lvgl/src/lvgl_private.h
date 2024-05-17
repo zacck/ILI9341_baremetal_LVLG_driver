647d2d9d116f820fb48b3c6c4e45b4a3fdc34774
Zacck Osiemo <coderv63@gmail.com>
2024-05-17 10:59:17 +0200

    Bump LVGL to the latest version so we can use new docs

---
+++ lvgl/src/lvgl_private.h
@@ -0,0 +1,44 @@
+/**
+ * @file lvgl_private.h
+ *
+ */
+
+#ifndef LVGL_PRIVATE_H
+#define LVGL_PRIVATE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "display/lv_display_private.h"
+#include "indev/lv_indev_private.h"
+#include "misc/lv_text_private.h"
+#include "libs/freetype/lv_freetype_private.h"
+#include "misc/cache/lv_cache_entry_private.h"
+#include "misc/cache/lv_cache_private.h"
+#include "drivers/windows/lv_windows_input_private.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*LVGL_PRIVATE_H*/
